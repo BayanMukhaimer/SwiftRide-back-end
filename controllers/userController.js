18ed91a9abd928e828921993e73f026ffbacf9f9
Bayan <bayanessam40@gmail.com>
2025-09-02 15:03:20 +0300

    register user

---
+++ controllers/userController.js
@@ -0,0 +1,53 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+ 
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role, phone, vehicle } = req.body;
+
+    
+    const vehicleData = role === "driver" ? vehicle : null;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role,
+      phone,
+      vehicle: vehicleData,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      vehicle: user.vehicle,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+ 
+
+
+
+module.exports = {
+  registerUser,
+  
+};
