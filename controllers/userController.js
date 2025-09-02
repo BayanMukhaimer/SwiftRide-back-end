3d2534a2e6f6c337cf084ab8376fd5d9a410a8ed
Bayan <bayanessam40@gmail.com>
2025-09-02 15:04:36 +0300

    login and get user

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,9 +45,47 @@ const registerUser = async (req, res) => {
 
  
 
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      vehicle: user.role === "driver" ? user.vehicle : undefined,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401).json({ message: "Invalid email or password" });
+  }
+};
+
+ 
+const getProfile = async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  
+  if (user.role !== "driver") {
+    user.vehicle = undefined;
+  }
+
+  res.json(user);
+};
+
+
 
 
 module.exports = {
   registerUser,
+  loginUser,
+  getProfile,
   
 };
